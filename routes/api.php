564fe867f1cf92e7270aec63056ee75c985a3c25
Noman Kabeer <44933511+nomankabeer@users.noreply.github.com>
2020-01-15 16:46:24 +0500

    routes

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
-
 Route::group(['namespace' => 'Api'], function() {
 	//ip address is an optional parameter for following routes.
 	// if api hit with the ip address then it will get the results based on ip address
